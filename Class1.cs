221bb73ec373b82073bd7471ec327f28d318fddb
Zobachov <zobachov@gmail.com>
2022-07-22 20:37:53 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // язык в языке.
+using System.Text; // Для работы со string
+using System.Threading.Tasks; // работа с многопоточностью
+
+// namespace - название проекта
+namespace _22._07._2022___Lesson2._Arrays
+{
+    // Участники соревнований по спортивному ориентированию
+   public class Uchastniki
+    {
+
+        string Vvod(string text)
+        {
+             Console.Write(text);
+            return Console.ReadLine();
+        }
+
+       public static int Vvod_age(string text)
+        {
+
+            try
+            {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.Write("Вы ввели что-то не то, попробуйте заново\n");
+                
+                return Vvod_age(text);
+            }
+        }
+
+            public Uchastniki(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public Uchastniki(int id)
+        {
+            Id = id;
+            Name = Vvod("Введите ФИО участника: ");
+            Age = Vvod_age("Введите возраст участника: ");
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID Участника: {Id} \nФИО Участника: {Name}\nВозраст Участника: {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _22._07._2022___Lesson2._Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<Uchastniki> uchastniki = new List<Uchastniki>();
+
+            int count = Uchastniki.Vvod_age("Введите кол-во участников: ");
+
+            for (int i = 0; i < count; i++)
+            {
+                uchastniki.Add(new Uchastniki(i+1));
+            }
+
+            void vyvod ()
+            {
+                Console.WriteLine("Все участники:");
+
+                foreach (var item in uchastniki)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Console.WriteLine("[1] Добавить участника");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        {
+                        uchastniki.Add(new Uchastniki(uchastniki.Count));
+                        /*Array.Resize(ref uchastniki, uchastniki.Length + 1);
+                        uchastniki[uchastniki.Length - 1] = new Uchastniki(uchastniki.Length);*/
+                        vyvod();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine();
+                            break;
+                        }
+                }
+            
+        }
+    }
+}
